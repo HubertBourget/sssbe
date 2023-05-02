a185649da299f1b26fcfa5f86b087123abc1a336
LogicRaysTechnology <biz@logicrays.com>
2023-05-02 18:54:51 +0530

    secured GCS creds file by adding it to .gitignore and used it's path from the .env file

--- utils/constants.js
+++ utils/constants.js
@@ -7,14 +7,8 @@ const { Storage } = require('@google-cloud/storage');
 // folder in which new created thumbnails will be stored for temperory time
 const thumbnailDir = './Thumbnails';
 
-// serviceKey is a .json file which contains google cloud storage credentials
-const serviceKey = path.join(__dirname, './sacred-sound-2a7ce18e134a.json');
-
-// create instance of storage with our credentials
-const storage = new Storage({
-    keyFilename: serviceKey,
-    projectId: process.env.PROJECT_ID,
-});    
+// create instance of storage with our credentials   
+const storage = new Storage();
 
 // getting google cloud storage bucket
 const bucket = storage.bucket(process.env.BUCKET_NAME);
