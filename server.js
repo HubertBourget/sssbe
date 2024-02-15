83ae38bbeb1c003da5d4adadc7d0398fb9af7981
Hubert Bourget <hubbourget@hotmail.com>
2024-02-15 15:07:43 -0600

    re-enabling staging origin all

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ const {
 express()
   .use(
     cors({
-      origin: ['https://sacredsound.app'], // "*"
+      origin: "*", // ['https://sacredsound.app']
       methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
     })
   )
