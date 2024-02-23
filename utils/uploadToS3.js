2f0b171607b70c531851047a5e5e07c2715e3518
Hubert Bourget <hubbourget@hotmail.com>
2024-02-22 20:03:03 -0600

    Making it better

--- utils/uploadToS3.js
+++ utils/uploadToS3.js
@@ -1,8 +1,5 @@
 const path = require("path");
-
-const storage = require('../utils/googleCloudStorage');
 const { bucket } = require('../utils/googleCloudStorage');
-
 
 module.exports = {
     /**
@@ -10,28 +7,33 @@ module.exports = {
      * @param, Array of string as a "url" parameter
      * @returns This will return the media url of images which are uploaded to the google cloud storage.
      */
-    upload: async function (url) {
-        try { 
-
+    upload: async function (urls) {
+        try {
             const folder = process.env.BUCKET_THUMBNAIL_FOLDER;
-
-            // create a unique file name with 6 digin random number and current date
-            const fileName = `${Math.random().toString().substr(2, 6)}--${new Date().toISOString()}--image.jpg`;
+            console.log("Uploading images to folder:", folder);
 
             let imgUrls = [];
-            for (let i = 0; i < url.length; i++) {
-                
-                // uploading images to the bucket
-                let data = await bucket.upload(url[i], {
+            for (let i = 0; i < urls.length; i++) {
+                const url = urls[i];
+                // Generating a unique file name
+                const fileName = `${Math.random().toString().substr(2, 6)}--${new Date().toISOString()}--image.jpg`;
+                console.log(`Uploading image ${i+1}/${urls.length}:`, url);
+                console.log("Generated fileName:", fileName);
+
+                // Attempting to upload images to the bucket
+                let data = await bucket.upload(url, {
                     destination: path.join(folder, fileName),
                 });
+                console.log(`Image ${i+1} uploaded. Media link:`, data[0].metadata.mediaLink);
 
                 imgUrls.push(data[0].metadata.mediaLink);
             }
 
+            console.log("All images uploaded successfully");
             return imgUrls;
         } catch(err) {
-            console.log("Err in File-uploadToS3 > Method-upload > :: ", err);
+            console.error("Error in upload function:", err);
+            throw err; // rethrow the error after logging
         }
     },
-}
+};
