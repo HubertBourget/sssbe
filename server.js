bfb8fee5292d23c0b1302b185cb6eda53e5d048d
Hubert Bourget <hubbourget@hotmail.com>
2024-03-12 17:09:49 -0600

    Making it better

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ express()
   // .post("/api/setUserOnRecombee/:userId", setUserOnRecombee) //For Manual Insertion Only!
 
   //MongoDB data management:
-  .post("/api/postNewContentTypePropertyWithAttributes", postNewContentTypePropertyWithAttributes)
+  // .post("/api/postNewContentTypePropertyWithAttributes", postNewContentTypePropertyWithAttributes)
 
   //Mux endpoint:
   .post("/api/postCreateLiveStream", postCreateLiveStream)
